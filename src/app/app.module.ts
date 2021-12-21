db0a53b14f6e2d95b5d68c76ea9324cee1702ff1
X147107 <ala.atrash-ext@socgen.com>
2021-12-21 13:51:02 +0100

    products module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,9 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { WelcomeComponent } from './welcome/welcome.component';
+import { ProductsModule } from './products/products.module'
 
 @NgModule({
   declarations: [
@@ -12,7 +12,8 @@ import { WelcomeComponent } from './welcome/welcome.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ProductsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/products/products.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ProductsListComponent } from './products-list/products-list.component';
+
+
+
+@NgModule({
+  declarations: [
+    ProductsListComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class ProductsModule { }
