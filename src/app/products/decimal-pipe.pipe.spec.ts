afe209dbf99602e0e2c9aa4dbfd18faa562236a4
X147107 <ala.atrash-ext@socgen.com>
2022-01-24 18:41:29 +0100

    decimal and currecy pipe

---
+++ src/app/products/decimal-pipe.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { DecimalPipePipe } from './decimal-pipe.pipe';
+
+describe('DecimalPipePipe', () => {
+  it('create an instance', () => {
+    const pipe = new DecimalPipePipe();
+    expect(pipe).toBeTruthy();
+  });
+});
